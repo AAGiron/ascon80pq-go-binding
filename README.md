a0b642e020ecb3bf4c81faa07a1ac3b7694b180c
alexandre.giron <alexandre.a.giron@gmail.com>
2022-09-22 17:08:44 -0300

    AEAD encrypt/decrypt binding
    It adds the bindings for both encrypting and decrypting with ascon-80pq functions. Also updates README with instructions and creates a LICENSE file.

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Ascon-80pq go-binding
-This repository offers Go bindings for the `ascon-80pq` C implementation. Ascon is one finalist of the Lightweight Cryptography standardization process conducted by NIST ([LWC](https://csrc.nist.gov/Projects/lightweight-cryptography/lwc-publications)). 
-
-## Usage
-
-To do.
+This repository offers a very simple Go binding for the `ascon-80pq` C implementation (aead_encrypt/decrypt operations). Ascon is one finalist of the Lightweight Cryptography standardization process conducted by NIST ([LWC](https://csrc.nist.gov/Projects/lightweight-cryptography/lwc-publications)). 
 
 ## Clone instructions
 
@@ -11,13 +7,23 @@ In a single line: `git clone --recursive https://github.com/AAGiron/ascon80pq-go
 
 Or use `cd ascon80pq-go-binding && git submodule update --init --recursive` after cloning.
 
-## Ascon-c compiling
+## Ascon-c building process
 
 In order to compile the required implementations for the binding, use:
 
 ```
 mkdir build && cd build
-#cmake .. -DALG_LIST="ascon80pq" -DIMPL_LIST="ref" -DTEST_LIST="genkat"
 cmake .. -DALG_LIST="ascon80pq" -DIMPL_LIST="ref" -DTEST_LIST="genkat" -DBUILD_SHARED_LIBS=ON
 cmake --build .
 ```
+
+Install it (and export) to `LD_LIBRARY_PATH` (e.g., `sudo cp *.so /usr/local/lib/` if that is your path).
+
+## Usage
+
+After cloning and building Ascon (above), test the binding using `go run test.go`. 
+
+
+## Disclaimer
+
+This is a very simple and third-party binding implementation: it does not offer warranty of any kind (nor any security guarantees).
--- test.go
+++ test.go
@@ -1,9 +1,3 @@
-//
-// main.go
-// Copyright (C) 2019 Tim Hughes
-//
-// Distributed under terms of the MIT license.
-//
 package main
 
 /*
@@ -19,104 +13,44 @@ import (
 )
 
 func main() {
-	//name := C.CString("Test")
-	//defer C.free(unsafe.Pointer(name))
-
-	//year := C.int(2022)
-
-	ptr := C.malloc(C.sizeof_char * 1024)
-	defer C.free(unsafe.Pointer(ptr))
-
-	/*unsigned char n[CRYPTO_NPUBBYTES] = {0, 1, 2,  3,  4,  5,  6,  7,
-                                       8, 9, 10, 11, 12, 13, 14, 15};
-  unsigned char k[CRYPTO_KEYBYTES] = {0, 1, 2,  3,  4,  5,  6,  7,
-                                      8, 9, 10, 11, 12, 13, 14, 15};
-  unsigned char a[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-  unsigned char m[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-  unsigned char c[32], h[32], t[32];
-  unsigned long long alen = 8;
-  unsigned long long mlen = 8;
-  unsigned long long clen = CRYPTO_ABYTES;
-  int result = 0;
-*/
+	//parameters from https://github.com/ascon/ascon-c/blob/main/tests/demo.c
 	n := [16]byte{0, 1, 2,  3,  4,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15}
 	k := [20]byte{0, 1, 2,  3,  4,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
-	//k := [16]byte{0, 1, 2,  3,  4,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15}
 	a := [16]byte{0, 1, 2,  3,  4,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15}
-	m := [16]byte{0, 1, 2,  3,  4,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15}
-	c := []byte{}
-	//h := []byte{}
-	//t := []byte{}
-	var alen uint64 = 8
-	var mlen uint64 = 8
-	var clen uint64 = 16
+	m := [16]byte{0, 1, 2,  3,  4,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15}		
+	var len uint64 = 16	
 
-	fmt.Println("Ascon-80pq binding test ---------------------- ")
-/*
-result |= crypto_aead_encrypt(c, &clen, m, mlen, a, alen, (void*)0, n, k);
-  print('c', c, clen - CRYPTO_ABYTES);
-  printf(" ");
-  print('t', c + clen - CRYPTO_ABYTES, CRYPTO_ABYTES);
-  printf(" -> ");
-  result |= crypto_aead_decrypt(m, &mlen, (void*)0, c, clen, a, alen, n, k);
-  print('a', a, alen);
-  printf(" ");
-  print('m', m, mlen);
-  printf("\n");
-
-  */
-  	pointClen := C.ulonglong(clen)
-  	pointMlen := C.ulonglong(mlen)
-  	sliceM := m[:]
-  	sliceA := a[:]
-  	sliceN := n[:]
-  	sliceK := k[:]
-  	zero := C.uchar(0)
+	fmt.Println("Ascon-80pq binding test --------------------------------- ")
+  	clen := C.ulonglong(16)
+  	mlen := C.ulonglong(len)
+  	alen := C.ulonglong(8)  	  
+  	zerobyte := C.uchar(0)
   	
-  	//pointers
-  	cp := (*C.uchar)(C.CBytes(c))
-  	sliceMp := (*C.uchar)(C.CBytes(sliceM))
-  	sliceAp := (*C.uchar)(C.CBytes(sliceA))
-  	sliceNp := (*C.uchar)(C.CBytes(sliceN))
-  	sliceKp := (*C.uchar)(C.CBytes(sliceK))
+  	//pointer conversions
+  	cbytes := (*C.uchar)(C.CBytes([]byte{}))
+  	mbytes := (*C.uchar)(C.CBytes(m[:]))
+  	abytes := (*C.uchar)(C.CBytes(a[:]))
+  	nbytes := (*C.uchar)(C.CBytes(n[:]))
+  	kbytes := (*C.uchar)(C.CBytes(k[:]))
+  	
   	//Encrypt
-	ret := C.crypto_aead_encrypt(cp, &pointClen, 
-			sliceMp, C.ulonglong(mlen), 
-			sliceAp, C.ulonglong(alen), &(zero), 
-			sliceNp, sliceKp)
+	ret := C.crypto_aead_encrypt(cbytes, &clen, mbytes, mlen, abytes, 
+								 alen, &(zerobyte), nbytes, kbytes)
 
-	//fmt.Println(cp)
-	fmt.Print("Message: ") //+ string(sliceM))
-	fmt.Printf("%v \n", m)
-	fmt.Print("Encryption code return (0 is success):")
-	fmt.Println(ret)
+	fmt.Printf("\tMessage:  %v \n", m)
+	fmt.Printf("Encryption return code (0 is success): %d\n", ret)
 
-	//Decrypt
+	//Decrypt in ptr	
+	ptr := C.malloc(C.sizeof_char * C.ulong(len))
+	defer C.free(unsafe.Pointer(ptr))
+	ret = C.crypto_aead_decrypt((*C.uchar)(ptr), &mlen, &(zerobyte), cbytes, 
+								clen, abytes, alen, nbytes, kbytes);
 	
-	ret = C.crypto_aead_decrypt( (*C.uchar)(ptr), &pointMlen, 
-	 							&(zero), cp, pointClen,  
-	 							sliceAp, C.ulonglong(alen),
-	 							sliceNp, sliceKp);
-	
-
-	if (ret == 0) {
-		//decrypted := C.GoBytes(ptr, sliceM) //Not ret
-		fmt.Print("Decrypted message:")
-		//fmt.Println(string(sliceA))
-		//fmt.Println(string(sliceM))	
-		//
-		b := C.GoBytes(ptr, 16)
-
+	if (ret == 0) {		
+		fmt.Print("\tDecrypted:")
+		b := C.GoBytes(ptr, C.int(len))
 		fmt.Println((b))
 	}else{
-		fmt.Print("Decryption code error: ")
-		fmt.Println(ret)
-	}
-	
+		fmt.Printf("Decryption code error: %d\n", ret)
+	}	
 }
-
-
-//PS: cgo LDFLAGS: -Lascon/build2 -llibcrypto_aead_ascon80pqv12_ref could be -L${SRCDIR}/libs or something
-//${SRCDIR}/ascon/build2
-//
-//cgo LDFLAGS: -L. -lcrypto_aead_ascon128v12_ref
